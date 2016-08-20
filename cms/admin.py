ffed203847d4315bdc8111c21950fec7041def33
ryujiin <ryujiin22@gmail.com>
2016-08-20 13:25:14 -0500

    Antes de modificar el archivo pages.js

--- cms/admin.py
+++ cms/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from models import *
+# Register your models here.
+
+class ImageCarruselInline(admin.TabularInline):
+	model = ImageCarrusel
+
+class BloqueAdmin(admin.ModelAdmin):
+	inlines = [ImageCarruselInline,]
+
+class LinkMenuInline(admin.TabularInline):
+	model = LinkMenu
+
+class MenuAdmin(admin.ModelAdmin):
+	inlines = [LinkMenuInline,]
+
+admin.site.register(Carrusel)
+admin.site.register(SeccionesCMS)
+admin.site.register(Page)
+admin.site.register(Bloque,BloqueAdmin)
+admin.site.register(ImageCarrusel)
+admin.site.register(TemplateBloque)
+admin.site.register(Menu,MenuAdmin)
